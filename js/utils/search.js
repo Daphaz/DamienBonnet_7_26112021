85c85977edefb6ee3fcda13da791ff2017c3c081
Damien Bonnet <bonnet.damien@ymail.com>
2022-02-07 13:09:13 +0100

    [:bug: FIX]  error description

--- js/utils/search.js
+++ js/utils/search.js
@@ -1,47 +1,47 @@
 const mainInputSearch = (receipes) => {
-	mainInput.addEventListener('input', (e) => {
-		if (e.target.value.length > 2) {
-			cardsContainer.innerHTML = '';
-			const query = e.target.value.toLowerCase();
+  mainInput.addEventListener('input', (e) => {
+    if (e.target.value.length > 2) {
+      cardsContainer.innerHTML = '';
+      const query = e.target.value.toLowerCase();
 
-			const results = receipes.filter((receipe) => {
-				return (
-					receipe.name.toLowerCase().startsWith(query) ||
-					receipe.description.toLowerCase().includes(query) ||
-					receipe.ingredients.some((ing) =>
-						ing.ingredient.toLowerCase().includes(query)
-					) ||
-					receipe.ustensils.some((ustensil) =>
-						ustensil.toLowerCase().includes(query)
-					) ||
-					receipe.appliance.toLowerCase().includes(query)
-				);
-			});
+      const results = receipes.filter((receipe) => {
+        return (
+          receipe.name.toLowerCase().startsWith(query) ||
+          receipe.description.toLowerCase().includes(query) ||
+          receipe.ingredients.some((ing) =>
+            ing.ingredient.toLowerCase().includes(query)
+          ) ||
+          receipe.ustensils.some((ustensil) =>
+            ustensil.toLowerCase().includes(query)
+          ) ||
+          receipe.appliance.toLowerCase().includes(query)
+        );
+      });
 
-			if (
-				!selectedIngredientFilters.length &&
-				!selectedApplianceFilters.length &&
-				!selectedUstensilFilters.length
-			) {
-				onFiltersCreateBody(results);
-				createCardReceipes(results);
-			} else {
-				onResearchFilter(results);
-			}
+      if (
+        !selectedIngredientFilters.length &&
+        !selectedApplianceFilters.length &&
+        !selectedUstensilFilters.length
+      ) {
+        onFiltersCreateBody(results);
+        createCardReceipes(results);
+      } else {
+        onResearchFilter(results);
+      }
 
-			if (!results.length) {
-				cardsContainer.append(
-					createDom(
-						'div',
-						`Aucune recette ne correspond à votre critère… vous pouvez
-     chercher « tarte aux pommes », « poisson », etc.`,
-						{ class: 'no__results' }
-					)
-				);
-			}
-		} else {
-			cardsContainer.innerHTML = '';
-			createCardReceipes(receipes);
-		}
-	});
+      if (!results.length) {
+        cardsContainer.append(
+          createDom(
+            'div',
+            `Aucune recette ne correspond à votre critère… vous pouvez
+        chercher « tarte aux pommes », « poisson », etc.`,
+            { class: 'no__results' }
+          )
+        );
+      }
+    } else {
+      cardsContainer.innerHTML = '';
+      createCardReceipes(receipes);
+    }
+  });
 };
